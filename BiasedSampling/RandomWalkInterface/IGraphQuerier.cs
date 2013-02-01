f9384979ab4015f5e83dca3b18de2e7a3045db66
apokryfos <apokryfos@outlook.com>
2013-02-01 17:32:04 +0000

    Added docs for the weights and graph querier
    Signed-off-by: apokryfos <apokryfos@outlook.com>

--- BiasedSampling/RandomWalkInterface/IGraphQuerier.cs
+++ BiasedSampling/RandomWalkInterface/IGraphQuerier.cs
@@ -4,31 +4,85 @@ using System.Linq;
 using System.Text;
 using QuickGraph;
 
-namespace RandomWalks.RandomWalkInterface
-{
-    public interface IGraphQuerier<TVertex, TEdge> : IDisposable        
-        where TEdge : IEdge<TVertex>
-    {
-        int TotalQueries { get; }
-        IEnumerable<TEdge> AdjecentEdges(TVertex vertex);
-        int AdjecentDegree(TVertex vertex);
-        TEdge AdjecentEdge(TVertex vertex, int index);
+namespace RandomWalks.RandomWalkInterface {
+	/// <summary>
+	/// General graph querier interface.
+	/// </summary>
+	/// <typeparam name="TVertex">Type of vertices (states)</typeparam>
+	/// <typeparam name="TEdge">Type of edges (transitions)</typeparam>
+	public interface IGraphQuerier<TVertex, TEdge> : IDisposable
+		where TEdge : IEdge<TVertex> {
+		/// <summary>
+		/// Should return total queries performed on the graph
+		/// </summary>
+		int TotalQueries { get; }
 
+		/// <param name="vertex">The state to be queried</param>
+		/// <returns>An enumerable of all available transitions from that state</returns>
+		IEnumerable<TEdge> AdjecentEdges(TVertex vertex);
+		/// <param name="vertex">The state to be queried</param>
+		/// <returns>The number of transitions that could be performed from that state </returns>
+		int AdjecentDegree(TVertex vertex);
+
+		/// <summary>
+		/// Assuming the transitions are ordered this should return the i-th transition from a state
+		/// </summary>
+		/// <param name="vertex">The state to be queried</param>
+		/// <param name="index">The transition index</param>
+		/// <returns>The i-th transition</returns>
+		/// <exception cref="IndexOutOfRangeException"/>
+		TEdge AdjecentEdge(TVertex vertex, int index);
+
+		/// <summary>
+		/// Should have the policy name. The key should be the short abbreviation of the name and the value should be the long name
+		/// </summary>
 		KeyValuePair<string, string> PolicyName { get; }
-    }
+	}
 
-    public interface IWeightedGraphQuerier<TVertex, TEdge> : IGraphQuerier<TVertex, TEdge>		
-        where TEdge : IEdge<TVertex>
-                       
-    {
+	/// <summary>
+	/// Weighted querier interface extension.
+	/// </summary>
+	/// <typeparam name="TVertex">Type of vertices (states)</typeparam>
+	/// <typeparam name="TEdge">Type of edges (transitions)</typeparam>
+	public interface IWeightedGraphQuerier<TVertex, TEdge> : IGraphQuerier<TVertex, TEdge>
+		where TEdge : IEdge<TVertex> {
 
-        TEdge WeightedAdjacentEdge(TVertex vertex, decimal weightedIndex);
-        decimal EdgeWeight(TEdge edge);
-        decimal VertexWeight(TVertex vertex);
-    }
+		/// <summary>
+		/// Assuming the transitions are ordered (irrespective of their weight)
+		/// General indexing is of the form: 
+		/// The first transition is always indexed between 0.0 (inclusive) and w(e1)/w(u) (exclusive)
+		/// where w(e1) is the transition weight of the first transition (e1) and w(u) is the vertex weight
+		/// the subsequent transitions are indexed from w(ei-1)/w(u) (inclusive) to w(ei)/w(u) (exclusive)
+		/// This is done in order to be used in conjuction with Random.NextDouble()
+		/// </summary>
+		/// <param name="vertex">The state to be queried</param>
+		/// <param name="weightedIndex">Weighted transition index. Should range from 0-1</param>
+		/// <returns>The transition which corresponds to the weighted index specified</returns>
+		/// <example> 
+		///		Assume a state with 3 transitions: 
+		///		Transition 1 has a weight 10.0
+		///		Transition 2 has a weight 5.0
+		///		Transition 3 has a weight 1.0
+		///		The weight of the state is 16.0
+		///		The weighted index which corresponds to transition 1 ranges from 0.0(inclusive) to 10.0/16.0 (exclusive)
+		///		Respectively the weighted index which corresponds to transition 2 ranges from 10.0/16.0 to (15.0/16.0)
+		///		etc.		
+		/// </example>
+		TEdge WeightedAdjacentEdge(TVertex vertex, decimal weightedIndex);
 
+		/// <summary>
+		/// Gets the weight if the specified transition
+		/// </summary>
+		/// <param name="edge">The transition to be queried</param>
+		/// <returns>The weight of the transition</returns>
+		decimal EdgeWeight(TEdge edge);
 
-   
-
-   
+		/// <summary>
+		/// Gets the weight if the specified state.
+		/// Typically the sum of all adjecent transition weights
+		/// </summary>
+		/// <param name="edge">The state to be queried</param>
+		/// <returns>The weight of the state</returns>
+		decimal VertexWeight(TVertex vertex);
+	}
 }
---
+++ BiasedSampling/Weights/WeightedRandomWalks.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using QuickGraph;
+using System.IO;
+using RandomWalks.Querier;
+
+
+
+namespace RandomWalks.Weights {
+	/// <summary>
+	/// Examples on how to use the <see cref="WeightFunction"/>
+	/// These methods generally provide an implementation for the weight function delegate
+	/// <para>
+	///	These weights were used in various experiments
+	/// </para>
+	/// </summary>
+	public static class HighDegreeBias {
+		public static decimal EdgeWeight<TVertex, TEdge>(IUndirectedGraph<TVertex, TEdge> targetGraph, TEdge e, double beta)
+		  where TEdge : IEdge<TVertex> { 
+			return (decimal)Math.Pow((targetGraph.AdjacentDegree(e.Source) * targetGraph.AdjacentDegree(e.Target)), beta);
+		}
+	}
+	
+	public class HiddenPartitionRandomWalkWeight<TVertex, TEdge>
+		where TEdge : IEdge<TVertex> {
+		private int vcount;
+		private decimal c;
+		public HiddenPartitionRandomWalkWeight(decimal c, int vcount) {
+			this.c = c;
+			this.vcount = vcount;
+		}
+		public decimal EdgeWeight(IGraph<TVertex, TEdge> targetGraph, TEdge e) {
+			return (1.0M + (Convert.ToInt32(e.Source) < vcount ? c : 0) + (Convert.ToInt32(e.Target) < vcount ? c : 0));
+		}
+	}
+
+	/// <summary>
+	/// General weight to use for weighted random walks.
+	/// Tag must be convertible to decimal. 
+	/// Tags are commonly (signed) real or natural numbers
+	/// If you deal with different weights consider creating another delegate implementation
+	/// </summary>
+	/// <typeparam name="TVertex">Vertex Type</typeparam>
+	/// <typeparam name="TEdge">Edge Type</typeparam>
+	/// <typeparam name="TTag">Edge tag type</typeparam>
+	public static class GeneralWeight<TVertex, TEdge, TTag>
+		where TEdge : IEdge<TVertex> 
+		where TEdge : ITagged<TTag> {
+		private int vcount;
+		private decimal c;
+
+		public decimal EdgeWeight(IGraph<TVertex, TEdge> targetGraph, TEdge e) {
+			return Convert.ToDecimal(e.Tag);
+		}
+	}
+
+	public static class VertexRandomWalkWeight {
+		public static decimal EdgeWeight<TVertex, TEdge>(IUndirectedGraph<TVertex, TEdge> targetGraph, TEdge e)
+			where TEdge : IEdge<TVertex> {
+			return ((1.0M / (decimal)targetGraph.AdjacentDegree(e.Source)) + (1.0M / (decimal)targetGraph.AdjacentDegree(e.Target)));
+		}
+	}
+
+	public static class TriangleRandomWalkWeight {
+		public static decimal EdgeWeight<TVertex, TEdge>(IUndirectedGraph<TVertex, TEdge> targetGraph, TEdge e)
+			where TEdge : IEdge<TVertex> {
+			if (e.Source.Equals(e.Target))
+				return 1.0M;
+
+			int t = 0;
+			HashSet<TVertex> hs = new HashSet<TVertex>();
+			foreach (var ed in targetGraph.AdjacentEdges(e.Source)) {
+				if (e.Equals(ed) || ed.Source.Equals(ed.Target))
+					continue;
+				hs.Add(ed.GetOtherVertex(e.Source));
+			}
+
+			foreach (var ed in targetGraph.AdjacentEdges(e.Target)) {
+				if (e.Equals(ed) || ed.Source.Equals(ed.Target))
+					continue;
+
+				if (hs.Contains(ed.GetOtherVertex(e.Target)))
+					t++;
+			}
+			return 1.0M + (decimal)t;
+		}
+
+	}
+}
